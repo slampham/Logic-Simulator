eed1398652c80ba65626051c20e45cb8af84245f
Eric Zhou <ericyuanzhou@gmail.com>
2020-11-20 14:23:43 -0800

    fixed constructor issues

--- src/org/ecs160/a1/NORGate.java
+++ src/org/ecs160/a1/NORGate.java
@@ -3,7 +3,7 @@ package org.ecs160.a1;
 import java.util.ArrayList;
 
 public class NORGate extends Gate{
-    public NORGate (ArrayList<Boolean> input, Boolean output) {
+    public NORGate (ArrayList<Boolean> input) {
         super(input, !(input.get(0) || input.get(1)));
     }
 }
--- src/org/ecs160/a1/NOTGate.java
+++ src/org/ecs160/a1/NOTGate.java
@@ -3,7 +3,7 @@ package org.ecs160.a1;
 import java.util.ArrayList;
 
 public class NOTGate extends Gate{
-    public NOTGate (ArrayList<Boolean> input, Boolean output) {
+    public NOTGate (ArrayList<Boolean> input) {
         super(input, !input.get(0));
     }
 }
