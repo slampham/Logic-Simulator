9ce6fb3ab893b014ec8d1a899a503b754b29a2b3
slampham <43860771+slampham@users.noreply.github.com>
2020-11-24 02:03:12 -0800

    Changed toggle to extend Button class. Also added gitignore

--- src/org/ecs160/a2/Toggle.java
+++ src/org/ecs160/a2/Toggle.java
@@ -1,13 +1,18 @@
 package org.ecs160.a2;
 
-public class Toggle {
-  public Boolean output = false;
+import com.codename1.ui.Button;
 
-  public void toggle() {
-    output = !output;
-  }
+public class Toggle extends Button {
+    public void toggle() {
+        if (getState() == STATE_PRESSED) {
+            released();
+        }
+        else {
+            pressed();
+        }
+    }
 
-  public Boolean output() {
-    return output;
-  }
+    public Boolean output() {
+        return getState() == STATE_PRESSED;
+    }
 }
